a58049e031baa97854bdc003d400db4c5895cecc
Ghullam Abbas <smilesiosteam@gmail.com>
2024-02-22 11:29:10 +0500

    feat: added base dependencies, os support , language versions and basic folder structure

---
+++ Sources/AboutSmiles/AboutSmilesTutorial/Views/ViewController/AboutSmilesTutorialViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutSmilesTutorialViewController.swift
+//  
+//
+//  Created by Ghullam  Abbas on 22/02/2024.
+//
+
+import UIKit
+
+class AboutSmilesTutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sources/AboutSmiles/Configurator/Configurator.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ghullam  Abbas on 22/02/2024.
+//
+
+import Foundation
+import UIKit
+
+public struct AboutSmilesConfigurator {
+    
+    public enum ConfiguratorType {
+        case aboutSmilesTutorial
+    }
+    
+    public static func create(type: ConfiguratorType) -> UIViewController {
+        switch type {
+        case .aboutSmilesTutorial:
+            return AboutSmilesTutorialViewController()
+        }
+    }
+    
+}
